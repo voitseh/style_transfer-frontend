29bc881adc24dcf0e866b100b3a007c9f0a5b19f
voitseh <voitseh.kaminsky@gmail.com>
2018-03-11 11:29:46 +0200

    add download bttn functionality

--- public/main.js
+++ public/main.js
@@ -1,7 +1,6 @@
-var uploadedPhoto = '';
-var checkedStyle = '';
+var uploadedPhoto = [];
+var checkedStyle = [];
 var client_data = {};
-
 ///////////////////utils/////////////////////////
 function sijax_data(key, value) { 
     Sijax.setRequestUri('http://93.77.34.47:63101/');
@@ -10,7 +9,6 @@ function sijax_data(key, value) {
     Sijax.request('client_data', [client_data]);
     delete client_data[key];
 }
-
 //////////// form submit settings ///////////////
 function uploadPhoto(drug = false, _file = null) {
     var photoImage = document.getElementById('photoImg');
@@ -24,31 +22,25 @@ function uploadPhoto(drug = false, _file = null) {
     var reader = new FileReader();
     reader.readAsDataURL(file);
     reader.addEventListener("load", function () {   
-        photoImage.setAttribute('src', reader.result);
-        //photoImage.width = 300;
-        //photoImage.height = 300;
+        photoImage.setAttribute('src', reader.result); 
         photoImage.style.borderRadius = '10%';
-        //photoImage.style.marginTop = '15px';
-        //photoImage.style.marginLeft = '15px';
-
-         uploadedPhoto = reader.result;
-        if(checkedStyle != ""){
+        uploadedPhoto.splice(0, uploadedPhoto.length);
+        uploadedPhoto.push(file.name)
+        uploadedPhoto.push(reader.result);
+        if(checkedStyle.length != 0){
             setBttnStylize_photoEnabled()
         }
        
     }, false);
-
     document.getElementById('photoCardMedia').style.display = 'none'
-
 }
-
-
 // implementation of image drag & drop functionality
 function handleFileSelect(evt) {
     evt.stopPropagation();
     evt.preventDefault();
+    if(evt.dataTransfer != undefined){
     var files = evt.dataTransfer.files; // FileList object.
-    uploadPhoto(true, files[0])
+    uploadPhoto(true, files[0])}
 }
 function handleDragOver(evt) {  
     evt.stopPropagation(); 
@@ -60,8 +52,6 @@ var dropZone = document.getElementById('drop_area');
 dropZone.addEventListener('dragover', handleDragOver, false);
 dropZone.addEventListener('drop', handleFileSelect, false);
 document.getElementById('photoFile').addEventListener('change', handleFileSelect, false);
-
-
 function uploadStyleFile() {
     var file = document.getElementById('styleFile').files[0];
     var reader = new FileReader();
@@ -72,19 +62,17 @@ function uploadStyleFile() {
 }
 function addStyle() {
     document.getElementById('styleFile').click();
-
 }
 // handle style check event
 $('body').on('click', 'img', function () {
-    checkedStyle = this.getAttribute("src");
-    if(uploadedPhoto != ""){                
+    checkedStyle.splice(0, checkedStyle.length);
+    checkedStyle.push(this.getAttribute("name"));
+    checkedStyle.push(this.getAttribute("src"));
+    if(uploadedPhoto.length != 0){                
         setBttnStylize_photoEnabled();
     }
    
 });
-
-
-
 // delete style gallery image on right click
 $(function () {
     $('ul').on('click', 'button', function (e) { //Get li under ul and invoke on contextmenu
@@ -97,15 +85,35 @@ $(function () {
         }
     });
 });
-
-
 //////////// fill styles gallery///////////////////
 window.onload = function () {
+    setBttnDownloadStylized_photoDisabled();
     sijax_data('style_gallery', 'style_gallery')
 }
-
 function onStylizeBttnClick(){
     document.getElementById('linearProgress').style.display = 'block'
-    sijax_data('photo', uploadedPhoto)
-    sijax_data('style', checkedStyle);
+    var data = [uploadedPhoto[0],uploadedPhoto[1],checkedStyle[0],checkedStyle[1]];
+    sijax_data('data', data);    
+    setBttnDownloadStylized_photoDisabled();
+    if( document.getElementById('stylized_img') != null){
+    document.getElementById('stylized_img').remove();
+    }
+   }
+function onStylizedImgLoadsHandler(event) {
+     var img = event.target;
+    if (event.target.localName == 'img') {
+     
+        img.style.width = '100%';
+        img.style.height =  '299px';       
+        img.style.borderRadius = '10%';
+         console.log(img)
+       }
+       setBttnDownloadStylized_photoEnabled();
+   };
+   //check stylized image loads event
+   document.getElementById("border_result")
+       .addEventListener('DOMNodeInserted', onStylizedImgLoadsHandler);
+
+function onDownloadStylized_photoBttnClick(){
+   console.log('Download click!')
 }
--- src/components/DownloadStylizedPhoto.js
+++ src/components/DownloadStylizedPhoto.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 
 const style = {
@@ -6,11 +6,35 @@ const style = {
   marginRight: 0
 };
 
-const RaisedButtonDownloadStylized_photo = () => (
-  <div>
-    <RaisedButton label="Download stylized photo!" disabled={true} style={style} />
-  </div>
-);
 
-export default RaisedButtonDownloadStylized_photo;
+window.setBttnDownloadStylized_photoEnabled = function setBttnDownloadStylized_photoEnabled() {  
+  RaisedButtonDownloadStylized_photo.setEnabled();
+}
+window.setBttnDownloadStylized_photoDisabled = function setBttnDownloadStylized_photoDisabled() { 
+  RaisedButtonDownloadStylized_photo.setDisabled();
+}
 
+export default class RaisedButtonDownloadStylized_photo extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      disabled: true,
+    }
+    RaisedButtonDownloadStylized_photo.setEnabled = RaisedButtonDownloadStylized_photo.setEnabled.bind(this);
+    RaisedButtonDownloadStylized_photo.setDisabled = RaisedButtonDownloadStylized_photo.setDisabled.bind(this);
+  }
+  static setEnabled() { 
+    this.setState({disabled: false});
+  }
+  static setDisabled() { 
+    this.setState({disabled: true});
+  }
+  render() {
+    return (
+      <div>
+        <RaisedButton label="Download stylized photo!" disabled={this.state.disabled} style={style} onClick={() => window.onDownloadStylized_photoBttnClick()} />
+      </div>
+    );
+  }
+}
